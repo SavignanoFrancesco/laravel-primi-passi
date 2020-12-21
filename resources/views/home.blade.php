473f311941eaeb99754d5dc72dcf95cebe367d60
Francesco <francesco.savignano96@gmail.com>
2020-12-21 17:47:48 +0100

    hompage 'hello' laravel impostata

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>laravel - primi - passi</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <h1>Hello Laravel!</h1>
+    </body>
+</html>
